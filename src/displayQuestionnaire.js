b979571633643e8d33791a3aade4482c1cfff169
elinorbengayev <elinorb240@gmail.com>
2022-07-03 19:47:47 +0300

    tailcale change

--- src/displayQuestionnaire.js
+++ src/displayQuestionnaire.js
@@ -4,12 +4,26 @@ let res = null;
 // fetch("./resources/PHQ9_SDC.json")
 //     .then(response => response.json())
 //     .then(json => LForms.Util.addFormToPage(json, 'formContainer'));
-
-const response = await fetch('https://3hxhninubinga65uw3i5jkpssm0bbgxs.lambda-url.us-east-1.on.aws/');
-// const response = await fetch('https://czp2w6uy37.execute-api.us-east-1.amazonaws.com/test/Questionnaire');
-const json = await response.json();
-LForms.Util.addFormToPage(json, 'formContainer')
-console.log(json);
+try {
+    let questionnaire_name = 'PHQ-9 quick depression assessment panel';
+    const response = await fetch("https://czp2w6uy37-vpce-0bdf8d65b826a59e3.execute-api.us-east-1.amazonaws.com/test/Questionnaire?questionnaire_name='PHQ-9 quick depression assessment panel'")
+    //     method: 'GET',
+    //     // body: JSON.stringify(res),
+    //     headers: {
+    //         "Access-Control-Allow-Headers" : "Content-Type",
+    //         "Access-Control-Allow-Origin": 'http://localhost:63342',
+    //         "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+    //     }
+    // });
+    // const response = await fetch('https://3hxhninubinga65uw3i5jkpssm0bbgxs.lambda-url.us-east-1.on.aws/');
+    // const response = await fetch('https://czp2w6uy37.execute-api.us-east-1.amazonaws.com/test/Questionnaire');
+    const json = await response.json();
+    console.log(json[0]);
+    LForms.Util.addFormToPage(json[0], 'formContainer')
+}
+catch (e){
+    console.log(e)
+}
 
 async function handleResponse(){
     res = LForms.Util.getFormFHIRData("QuestionnaireResponse", "R4");
@@ -22,9 +36,12 @@ async function handleResponse(){
     console.log(res);
     // textFileAsBlob = new Blob([JSON.stringify(res,null,4)], {type : 'application/json'});
 
-    const url = new URL("https://i4nyjmf3odmdjgeu6jnq6owfw40skwxf.lambda-url.us-east-1.on.aws/");
-    url.search = new URLSearchParams(res);
-    const response = await fetch(url);
+    // const result = fetch('https://czp2w6uy37-vpce-0bdf8d65b826a59e3.execute-api.us-east-1.amazonaws.com/test/questionnaireResponse', {
+    //     method: 'POST',
+    //     body: JSON.stringify(res),
+    //     headers: { 'Content-Type': 'application/json' }
+    // }).then(res => res.json())
+    //     .then(json => console.log(json));
 
     // const a = document.createElement("a");
     // const file = new Blob([JSON.stringify(res,null,4)], {type : 'application/json'});
